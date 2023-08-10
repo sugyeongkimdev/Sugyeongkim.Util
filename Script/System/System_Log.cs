2708dab3d6999012221d377bdaa790c707573780
DESKTOP-BTRAG5Qhomehome <sugyeongkim.dev@gmail.com>
2023-08-10 16:24:43 +0900

    콘솔에서 로그로 변경

---
+++ Script/System/System_Log.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+// 호출자 정보 추적 특성을 이용한 로그 시스템
+namespace BigUtil
+{
+    public static class Log
+    {
+        //==========================================================//
+
+        // Conditional ("UNITY_EDITOR")]
+        public static void Str (object logStr = default,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            string logHeader = $"[{GetPathLastString (sourceFilePath).ToColor (Color.green)}]";
+            Debug.Log ($"{logHeader} : {logStr}");
+        }
+
+        // [Conditional ("UNITY_EDITOR")]
+        public static void Error (object logStr = default,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            string logHeader = $"[{GetPathLastString (sourceFilePath).ToColor (Color.red)}]";
+            Debug.LogError ($"{logHeader} : {logStr}");
+        }
+
+        // [Conditional ("UNITY_EDITOR")]
+        public static void Exception (Exception e,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            //string logHeader = $"[{GetPathLastString (sourceFilePath).ToColor (Color.red)}]";
+            Debug.LogException (e);
+        }
+
+        //==========================================================//
+
+        // 파일 경로 잘라서 마지막 가져오기
+        private static string GetPathLastString (string path)
+        {
+            return path.Split ('\\').Last ().Split ('.').First ();
+        }
+
+        // string rich text, 색상 서식 칠하기
+        private static string ToColor (this string str, Color color = new Color ())
+        {
+            string hexColorStr = color.a == 0 ? "red" : $"#{ColorUtility.ToHtmlStringRGBA (color)}";
+            return $"<color={hexColorStr}>{str}</color>";
+        }
+
+        //==========================================================//
+
+    }
+}
