52f8976722f5d9179986f45a1f48e725967a87ac
sugyeongkim <sugyeongkim.dev@gmail.com>
2023-12-18 16:59:32 +0900

    bootstrap 기본틀 구성

---
+++ Project/Script/System/Scene/Bootstrap.cs
@@ -0,0 +1,41 @@
+using System;
+using UniRx;
+
+namespace SugyeongKim.Util
+{
+    public class Bootstrap : GlobalSingleton<Bootstrap>
+    {
+        // 프로그램 '최초 실행시' 어느씬이든 초기화 가능하도록 초기화를 모아둠, scene 최초 진입시 실행하면 됨
+        // 다른 bootstrap class를 생성해서 overriding 해서 사용바람 (BootstrapTemplate.txt 참조)
+        protected static bool isBootstrapInit = false;
+        public virtual IObservable<Unit> BootstrapInit ()
+        {
+            if (isBootstrapInit)
+            {
+                return Observable.ReturnUnit ();
+            }
+            else
+            {
+                isBootstrapInit = true;
+                return Observable.ReturnUnit ()
+                    .DoOnSubscribe (() => UtilLog.Log ("Bootstrap start."))
+                    .DoOnTerminate (() => UtilLog.Log ("Bootstrap done."))
+
+                    // 기본 프로그램 초기화
+                    .Do (_ =>
+                    {
+#if UNITY_IOS || UNITY_ANDROID
+                        //Application.targetFrameRate = 60;
+#else
+                    //QualitySettings.vSyncCount = 1;
+#endif
+                        //Screen.sleepTimeout = SleepTimeout.NeverSleep;
+                        //Screen.SetResolution ((int)Screen.width, (int)Screen.height, true);
+                    })
+
+                    // 싱글톤 초기화
+                    .SelectMany (_ => SingletonTool.InitSingletonAsObservable ());
+            }
+        }
+    }
+}
