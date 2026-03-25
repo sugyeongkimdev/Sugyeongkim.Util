d6475c843ff2b8fd860b2641d686b1ada0fe996c
sugyeongkimdev <51020780+sugyeongkimdev@users.noreply.github.com>
2026-03-26 03:00:18 +0900

    ResourceManager and GlobalCanvasUIManager utility update

--- Project/Util.Script/System/ResourceManager.cs
+++ Project/Util.Script/System/ResourceManager.cs
@@ -4,16 +4,36 @@ namespace SugyeongKim.Util
 {
     public class ResourceManager : GlobalSingleton<ResourceManager>
     {
-        // Start is called before the first frame update
-        void Start ()
-        {
+        private System.Collections.Generic.Dictionary<string, GameObject> prefabCache = new System.Collections.Generic.Dictionary<string, GameObject>();
 
+        public GameObject LoadPrefab(string path)
+        {
+            if (prefabCache.TryGetValue(path, out var prefab))
+            {
+                return prefab;
+            }
+
+            prefab = Resources.Load<GameObject>(path);
+            if (prefab != null)
+            {
+                prefabCache[path] = prefab;
+            }
+            else
+            {
+                DEBUG.Error($"[ResourceManager] 리소스를 찾을 수 없습니다: {path}");
+            }
+
+            return prefab;
         }
 
-        // Update is called once per frame
-        void Update ()
+        public GameObject InstantiatePrefab(string path, Vector3 position = default, Quaternion rotation = default, Transform parent = null)
         {
-
+            var prefab = LoadPrefab(path);
+            if (prefab != null)
+            {
+                return Object.Instantiate(prefab, position, rotation, parent);
+            }
+            return null;
         }
     }
 }
